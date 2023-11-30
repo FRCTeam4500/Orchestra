0a4708cfa58a6a1a9c1d6241f731df93b4e01c4e
TruestGecko <92557966+VimalBuckley@users.noreply.github.com>
2023-11-29 21:49:47 -0600

    Maybe can add files without changing code ??

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import java.io.FilenameFilter;
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -11,15 +15,35 @@ public class RobotContainer {
     private SendableChooser<String> songSelector;
 
 	public RobotContainer() {
-        xbox = new CommandXboxController(2);
         setupSongSelection();
         setupOrchestra(2, 3, 4, 5, 6, 7, 8, 9);
     }
-
-    public void setupOrchestra(int... motorIDs) {
-        orchestraCommand = new OrchestraCommand(
-            motorIDs
+    
+    private String[] getChrpFileNames() {
+        return Filesystem.getDeployDirectory().list(
+            new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(".chrp");
+                }
+            }
         );
+    }
+
+    private void setupSongSelection() {
+        songSelector = new SendableChooser<String>();
+        songSelector.setDefaultOption("None", null);
+        for (String fileName : getChrpFileNames()) {
+            songSelector.addOption(
+                fileName.split("\\.(?=[^\\.]+$)")[0], 
+                fileName
+            );
+        }
+        Shuffleboard.getTab("Song Selection Screen").add("Song Selector", songSelector);
+    }
+
+    private void setupOrchestra(int... motorIDs) {
+        xbox = new CommandXboxController(2);
+        orchestraCommand = new OrchestraCommand(motorIDs);
 
         xbox.start().onTrue(orchestraCommand);
 
@@ -30,22 +54,5 @@ public class RobotContainer {
         xbox.y().onTrue(Commands.runOnce(
             () -> orchestraCommand.stop()
         ));
-    }
-
-    public void setupSongSelection() {
-        songSelector = new SendableChooser<String>();
-        songSelector.setDefaultOption("Output", "output.chrp");
-        songSelector.addOption("Song 1", "song1.chrp");
-        songSelector.addOption("Song 2", "song2.chrp");
-        songSelector.addOption("Song 3", "song3.chrp");
-        songSelector.addOption("Song 4", "song4.chrp");
-        songSelector.addOption("Song 5", "song5.chrp");
-        songSelector.addOption("Song 6", "song6.chrp");
-        songSelector.addOption("Song 7", "song7.chrp");
-        songSelector.addOption("Song 8", "song8.chrp");
-        songSelector.addOption("Song 9", "song9.chrp");
-        songSelector.addOption("Song 10", "song10.chrp");
-        songSelector.addOption("Song 11", "song11.chrp");
-        Shuffleboard.getTab("Song Selection Screen").add("Song Selector", songSelector);
-    }
+    } 
 }
