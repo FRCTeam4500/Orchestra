81b676cf096710da5b53bccf4c160bf8ea25e3dd
Vimal Buckley <vbuckley68@claytonschools.net>
2025-01-15 16:39:22 -0600

    Updated to 2025

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,7 @@ public class RobotContainer {
 
 	public RobotContainer() {
         setupSongSelection();
-        setupOrchestra(2, 3, 4, 5, 6, 7, 8, 9);
+        setupOrchestra(2, 4, 6, 8);
     }
     
     private String[] getChrpFileNames() {
